5fdca3d16e5c279b42e27b2f41bff67cdf9f9407
Sendy Halim <sendyhalim93@gmail.com>
2017-06-04 23:55:00 +0700

    Make URL to conform Argo Decodable protocol

---
+++ Yomu/Extensions/URL.swift
@@ -0,0 +1,25 @@
+//
+//  URL.swift
+//  Yomu
+//
+//  Created by Sendy Halim on 6/4/17.
+//  Copyright © 2017 Sendy Halim. All rights reserved.
+//
+
+import Argo
+import Swiftz
+
+extension URL: Decodable {
+  public static func decode(_ json: JSON) -> Decoded<URL> {
+    switch json {
+    case JSON.string(let url):
+      return URL(string: url).map(pure) ?? .typeMismatch(
+        expected: "A String that is convertible to URL",
+        actual: url
+      )
+
+    default:
+      return .typeMismatch(expected: "String", actual: json)
+    }
+  }
+}
