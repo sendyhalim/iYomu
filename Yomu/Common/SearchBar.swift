8fcc7b715b8a5152434e422a60d47a8a375065e0
Sendy Halim <sendyhalim93@gmail.com>
2018-02-09 01:09:51 +0700

    Add left padding between UISearchBar search icon with text

--- Yomu/Common/SearchBar.swift
+++ Yomu/Common/SearchBar.swift
@@ -10,15 +10,20 @@ import Foundation
 import UIKit
 
 class SearchBar: UISearchBar {
-
   override func willMove(toSuperview newSuperview: UIView?) {
     super.willMove(toSuperview: newSuperview)
 
-    setImage(#imageLiteral(resourceName: "search"), for: .search, state: .normal)
     searchBarStyle = .minimal
 
-    let textField = value(forKey: "_searchField") as! UITextField
+    // Create search icon
+    let searchIcon = UIImageView(image: #imageLiteral(resourceName: "search"))
+    let searchImageSize = searchIcon.image!.size
+    searchIcon.frame = CGRect(x: 0, y: 0, width: searchImageSize.width + 10, height: searchImageSize.height)
+    searchIcon.contentMode = UIViewContentMode.center
 
+    // Configure text field
+    let textField = value(forKey: "_searchField") as! UITextField
+    textField.leftView = searchIcon
     textField.borderStyle = .none
     textField.backgroundColor = UIColor(hex: "#F7F7F7")
     textField.clipsToBounds = true
